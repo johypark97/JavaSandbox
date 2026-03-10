dd6740db675f763abcf48ee614c0e569b901ec6b
johypark97 <johypark97@gmail.com>
2026-03-10 21:14:01 +0900

    Fix some Gradle warnings related to Mockito

--- buildSrc/src/main/kotlin/buildlogic.java-common-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/buildlogic.java-common-conventions.gradle.kts
@@ -74,6 +74,13 @@ pmd {
 }
 
 tasks.named<Test>("test") {
-    jvmArgs.add("-javaagent:${mockitoAgent.asPath}")
+    // Suppress the following warnings, but may degrade app startup performance.
+    // `OpenJDK 64-Bit Server VM warning: Sharing is only supported for boot loader classes because bootstrap classpath has been appended`
+    jvmArgs("-Xshare:off")
+
+    // Add the Mockito-core as a Java agent.
+    // Note: `jvmArgs.add()` is not working.
+    jvmArgs("-javaagent:${mockitoAgent.asPath}")
+
     useJUnitPlatform()
 }
